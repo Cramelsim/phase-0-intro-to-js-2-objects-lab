298ff85922fb41287646ec491db1e401b0f75774
MELODY SIMWA <simwamelody@gmail.com>
2024-11-20 09:32:02 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee= {
+    employeeName:"Melody",
+    streetAddress:"24 Nairobi"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return {
+    ...employee, 
+    [key]: value 
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee;
+}
+/*deletes `key` from a clone of employee and returns the new employee*/
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
